96dd44cb81c5e56ac8379cb80c6f04508a3493e1
Dillon <Dillon@DillonsPC>
2019-01-14 21:53:38 +0100

    Core Work
    ParallaxStarter v0.2;0.3;0.4
    Tree v0.2;0.3

---
+++ Parallaxe/src/ch/elste/parallaxe/ParallaxStarter.java
@@ -0,0 +1,228 @@
+package ch.elste.parallaxe;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+
+/**
+ * The starter class for the parallax program.
+ * 
+ * @author Dillon
+ * 
+ * @version 0.4
+ *
+ */
+public class ParallaxStarter implements Runnable {
+	public static volatile boolean rendering = true;
+
+	private static final ParallaxStarter instance = new ParallaxStarter();
+
+	/**
+	 * The speed of the camera in pixels per second.
+	 */
+	private static double speed = 600d;
+
+	private final JFrame frame;
+	private final JPanel panel;
+	private Tree[] tree;
+	private BufferedImage currFrame;
+
+	private long frameStartTime, frameTime, timeSinceFrameChange, timeAtFrameChange;
+	private static final long FIXED_FRAME_TIME_NANOS = (long) (1d / 60d * Math.pow(10, 9));
+
+	/**
+	 * @since 0.1
+	 */
+	private ParallaxStarter() {
+		frame = new JFrame("Parallax");
+		panel = new JPanel();
+
+		panel.setPreferredSize(new Dimension(1600, 800));
+		frame.setContentPane(panel);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				rendering = false;
+				super.windowClosing(e);
+				System.exit(0);
+			}
+		});
+
+		currFrame = new BufferedImage(panel.getWidth(), panel.getHeight(), BufferedImage.TYPE_INT_RGB);
+	}
+
+	/**
+	 * @since 0.1
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(instance);
+	}
+
+	@Override
+	public void run() {
+		tree = new Tree[150];
+		for (int i = 0; i < tree.length; i++) {
+			tree[i] = new Tree(Math.random() * panel.getWidth(), Math.random() * 10, Math.random() * 950 + 50);
+		}
+		sortTrees();
+
+		SwingWorker<Void, Void> renderThread = new SwingWorker<Void, Void>() {
+
+			@Override
+			protected Void doInBackground() throws Exception {
+				while (rendering) {
+					if (timeSinceFrameChange > FIXED_FRAME_TIME_NANOS)
+						update(FIXED_FRAME_TIME_NANOS);
+
+					frameStartTime = System.nanoTime();
+
+					render(currFrame.createGraphics());
+
+					frameTime = System.nanoTime() - frameStartTime;
+
+					if (timeSinceFrameChange > FIXED_FRAME_TIME_NANOS) {
+						panel.getGraphics().drawImage(currFrame, 0, 0, panel);
+						timeSinceFrameChange = 0;
+						timeAtFrameChange = System.nanoTime();
+					}
+					
+					timeSinceFrameChange = System.nanoTime() - timeAtFrameChange;
+				}
+				return null;
+			}
+		};
+
+		renderThread.execute();
+	}
+
+	/**
+	 * Update the physics.
+	 * 
+	 * @param elapsedTime
+	 *            the time since last physics update in nanoseconds
+	 * 
+	 * @since 0.4
+	 */
+	public void update(double elapsedTime) {
+		for (int i = 0; i < tree.length; i++) {
+			tree[i].translateX(-speed * elapsedTime / Math.pow(10, 9));
+		}
+	}
+
+	/**
+	 * The render method, to render a frame and save it to {@code g2d}.
+	 * 
+	 * @param g2d
+	 *            the {@link Graphics2D} object to draw to
+	 * 
+	 * @since 0.1
+	 */
+	public void render(Graphics2D g2d) {
+		g2d.clearRect(0, 0, currFrame.getWidth(), currFrame.getHeight());
+		g2d.setBackground(Color.BLACK);
+
+		for (int i = 0; i < tree.length; i++) {
+			tree[i].render(g2d);
+		}
+
+		g2d.drawString(String.format("FPS: %4.1f", getFPS()), 5, 20);
+	}
+
+	/**
+	 * Returns the time having been needed to render the last frame.
+	 * 
+	 * @return the last frame time in nanoseconds
+	 * 
+	 * @since 0.2
+	 */
+	public long getFrameTimeNanos() {
+		return frameTime;
+	}
+
+	/**
+	 * Returns the time having been needed to render the last frame.
+	 * 
+	 * @return the last frame time in seconds
+	 * 
+	 * @since 0.2
+	 * 
+	 * @see #getFrameTimeNanos()
+	 */
+	public double getFrameTime() {
+		return frameTime / Math.pow(10, 9);
+	}
+
+	/**
+	 * Returns the frames per second.
+	 * 
+	 * @return the frames per second
+	 * 
+	 * @since 0.2
+	 */
+	public double getFPS() {
+		return 1d / getFrameTime();
+	}
+
+	/**
+	 * Returns the current frame as a {@link BufferedImage}.
+	 * 
+	 * @return the current frame
+	 * 
+	 * @since 0.2
+	 */
+	public BufferedImage getCurrFrame() {
+		return currFrame;
+	}
+
+	/**
+	 * Returns an instance of this class.
+	 * 
+	 * @return an instance of this class
+	 * 
+	 * @since 0.2
+	 */
+	public static ParallaxStarter instance() {
+		return instance;
+	}
+
+	/**
+	 * Sort the trees from biggest z to smallest.
+	 * 
+	 * @since 0.3
+	 */
+	private void sortTrees() {
+		for (int i = 1; i < tree.length; i++) {
+			for (int j = i; j > 0 && tree[j - 1].getZ() < tree[j].getZ(); j--) {
+				swap(j, j - 1);
+			}
+		}
+	}
+
+	/**
+	 * Swap to object in the tree array.
+	 * 
+	 * @param i
+	 *            the first index
+	 * @param j
+	 *            the second index
+	 * 
+	 * @since 0.3
+	 */
+	private void swap(int i, int j) {
+		Tree temp = tree[i];
+		tree[i] = tree[j];
+		tree[j] = temp;
+	}
+}
--- Parallaxe/src/ch/elste/parallaxe/Tree.java
+++ Parallaxe/src/ch/elste/parallaxe/Tree.java
@@ -1,38 +1,406 @@
 package ch.elste.parallaxe;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.geom.Point2D;
 
+/**
+ * A tree with x, y and z coordinates.
+ * 
+ * @author Dillon
+ *
+ * @version 0.3
+ */
 public class Tree {
-	private int x, y, z;
-	private Polygon[] polys;
+	public static final double LEAVE_WIDTH = 800;
+	public static final double LEAVE_HEIGHT = 200;
+	public static final double TRUNK_WIDTH = 240;
+	public static final double TRUNK_HEIGHT = 390;
 
-	public Tree(int x, int y, int z) {
+	private double x, y, z;
+	private Leaves[] leaves;
+	private Rectangle trunk;
+
+	/**
+	 * Creates a new tree object with given values.
+	 * 
+	 * @param x
+	 *            the x-coordinate
+	 * @param z
+	 *            the y-coordinate between 0 and 1000
+	 * 
+	 * @throws IllegalArgumentException
+	 * 
+	 * @since 0.1
+	 */
+	public Tree(double x, double y, double z) {
+		if (z < 0 || z > 1000)
+			throw new IllegalArgumentException("z must be between 0 and 1000");
+
 		this.x = x;
-		this.y = y;
 		this.z = z;
-		
-		polys = new Polygon[4];
-		polys[0] = new Polygon();
-		polys[0].addPoint(getX(0), 200);
-		polys[0].addPoint(getX(50), 200);
-		polys[0].addPoint(getX(50), 150);
-		polys[0].addPoint(getX(0), 150);
-		for(int i = 1; i < polys.length; i++) {
-			polys[i] = new Polygon();
-			polys[i].addPoint(getX(0), 50*i);
-			polys[i].addPoint(getX(50), 50*i);
-			polys[i].addPoint(getX(25), 50*(i - 1));
+		this.y = ParallaxStarter.instance().getCurrFrame().getHeight() / 2d - y;
+
+		leaves = new Leaves[3];
+
+		trunk = new Rectangle((int) Math.round(getX((getLeaveWidth() - getTrunkWidth()) / 2)),
+				(int) Math.round(getY(leaves.length * getLeaveHeight())), (int) Math.round(getTrunkWidth()),
+				(int) Math.round(getTrunkHeight()));
+
+		leaves[0] = new Leaves(getX(), getY(), getLeaveWidth(), getLeaveHeight(), .5);
+		for (int i = 1; i < leaves.length; i++) {
+			leaves[i] = new Leaves(getX(), getY(getLeaveHeight() * i), getLeaveWidth(), getLeaveHeight(), .2);
+			leaves[i].setPos(getX(), getY(getLeaveHeight() * i));
 		}
 	}
 
+	/**
+	 * Render this tree.
+	 * 
+	 * @param g2d
+	 *            the {@link Graphics2D} object to draw to
+	 * 
+	 * @since 0.1
+	 */
 	public void render(Graphics2D g2d) {
-		for(Polygon p : polys) {
-			g2d.fillPolygon(p);
+		g2d.setColor(new Color(112, 43, 26));
+		g2d.fill(trunk);
+
+		for (Leaves l : leaves) {
+			l.render(g2d);
+		}
+
+		g2d.setColor(Color.WHITE);
+	}
+
+	/**
+	 * @since 0.2
+	 */
+	private void updatePolys() {
+		if (x < -getLeaveWidth())
+			x = ParallaxStarter.instance().getCurrFrame().getWidth();
+
+		trunk = new Rectangle((int) Math.round(getX((getLeaveWidth() - getTrunkWidth()) / 2)),
+				(int) Math.round(getY(leaves.length * getLeaveHeight())), (int) Math.round(getTrunkWidth()),
+				(int) Math.round(getTrunkHeight()));
+
+		for (int i = 0; i < leaves.length; i++) {
+			leaves[i].setPos(getX(), getY(getLeaveHeight() * i));
 		}
 	}
-	
-	private int getX(int offset) {
-		return x+offset;
+
+	/**
+	 * Returns the {@link #x x-coordinate}.
+	 * 
+	 * @return the x-coordinate
+	 * 
+	 * @since 0.2
+	 */
+	public double getX() {
+		return x;
+	}
+
+	/**
+	 * Returns the {@link #x x-coordinate} plus the {@code offset}.
+	 * 
+	 * @param offset
+	 *            the offset to be added to x
+	 * 
+	 * @return the new coordinate
+	 * 
+	 * @since 0.1
+	 */
+	private double getX(double offset) {
+		return x + offset;
+	}
+
+	/**
+	 * Sets the {@link #x x-coordinate}.
+	 * 
+	 * @param x
+	 *            the coordinate
+	 * 
+	 * @since 0.2
+	 */
+	public void setX(double x) {
+		this.x = x;
+		updatePolys();
+	}
+
+	/**
+	 * Translates this tree by {@code x}.
+	 * 
+	 * @param x
+	 *            the value to translate by
+	 * 
+	 * @since 0.3
+	 */
+	public void translateX(double x) {
+		this.x += x / getSpeedFactor(z);
+		updatePolys();
+	}
+
+	/**
+	 * Returns the {@link #y y-coordinate of this tree}.
+	 * 
+	 * @return the y-coordinate
+	 * 
+	 * @since 0.2
+	 */
+	public double getY() {
+		return y;
+	}
+
+	/**
+	 * Returns the {@link #y y-coordinate} plus the {@code offset}.
+	 * 
+	 * @param offset
+	 *            the offset to be added to y
+	 * 
+	 * @return the new coordinate
+	 * 
+	 * @since 0.1
+	 */
+	private double getY(double offset) {
+		return y + offset;
+	}
+
+	/**
+	 * Sets the {@link #y y-coordinate}.
+	 * 
+	 * @param y
+	 *            the coordinate
+	 * 
+	 * @since 0.2
+	 */
+	public void setY(double y) {
+		this.y = y;
+		updatePolys();
+	}
+
+	/**
+	 * Translates this tree by {@code y}.
+	 * 
+	 * @param y
+	 *            the value to translate by
+	 * 
+	 * @since 0.3
+	 */
+	public void translateY(double y) {
+		this.y += y / getSpeedFactor(z);
+		updatePolys();
+	}
+
+	/**
+	 * Returns the z-coordinate.
+	 * 
+	 * @return the z-coordinate
+	 * 
+	 * @since 0.3
+	 */
+	public double getZ() {
+		return z;
+	}
+
+	/**
+	 * Returns the height of this tree object.
+	 * 
+	 * @return the height of this tree object
+	 * 
+	 * @since 0.3
+	 */
+	public double getTreeHeight() {
+		return getLeaveHeight() * 3 + getTrunkHeight();
+	}
+
+	/**
+	 * Returns the leave height with respect to {@code z}.
+	 * 
+	 * @return the leave height with respect to {@code z}.
+	 * 
+	 * @since 0.3
+	 */
+	private double getLeaveHeight() {
+		return LEAVE_HEIGHT / getHeightFactor(z);
+	}
+
+	/**
+	 * Returns the trunk height with respect to {@code z}.
+	 * 
+	 * @return the trunk height with respect to {@code z}.
+	 * 
+	 * @since 0.3
+	 */
+	private double getTrunkHeight() {
+		return TRUNK_HEIGHT / getHeightFactor(z);
+	}
+
+	/**
+	 * Returns the leave width with respect to {@code z}.
+	 * 
+	 * @return the leave width with respect to {@code z}.
+	 * 
+	 * @since 0.3
+	 */
+	private double getLeaveWidth() {
+		return LEAVE_WIDTH / getHeightFactor(z);
+	}
+
+	/**
+	 * Returns the trunk width with respect to {@code z}.
+	 * 
+	 * @return the trunk width with respect to {@code z}.
+	 * 
+	 * @since 0.3
+	 */
+	private double getTrunkWidth() {
+		return TRUNK_WIDTH / getHeightFactor(z);
+	}
+
+	/**
+	 * Return the factor for the calculation of height and width.
+	 * 
+	 * @param x
+	 *            the x-value to calculate the width with.
+	 * 
+	 * @return
+	 *         <li>{@code 1} for {@code x = 0}</li>
+	 *         <li>{@code 100} for {@code x = 1000}</li>
+	 * 
+	 * @since 0.3
+	 */
+	private double getHeightFactor(double x) {
+		return 0.099 * x + 1d;
+	}
+
+	/**
+	 * Return the factor for the speed calculation
+	 * 
+	 * @param x
+	 *            the x-value
+	 * 
+	 * @return
+	 *         <li>{@code 1} for {@code x = 0}</li>
+	 *         <li>{@code 100} for {@code x = 1000}</li>
+	 * 
+	 * @since 0.3
+	 */
+	private double getSpeedFactor(double x) {
+		return 0.01 * x + 1d;
+	}
+
+	/**
+	 * A class to hold information about the leaves of the tree.
+	 * 
+	 * @author Dillon
+	 * 
+	 * @since 0.3
+	 * @version 1.1
+	 *
+	 */
+	private static class Leaves {
+		private Polygon p;
+		private Color color, temp;
+		private Point2D.Double pos, size;
+		private double topWidthPercent;
+
+		/**
+		 * Cretes new leaves with given values.
+		 * 
+		 * @param x
+		 *            x-coordinate
+		 * @param y
+		 *            y-coordinate
+		 * @param width
+		 *            the bottom width of the leaves
+		 * @param height
+		 *            the height of the leaves
+		 * @param topWidthPercent
+		 *            the top width in percent of {@code width} between 0 and 1
+		 * 
+		 * @since 1.0
+		 */
+		public Leaves(double x, double y, double width, double height, double topWdithPercent) {
+			this(Color.GREEN, x, y, width, height, topWdithPercent);
+		}
+
+		/**
+		 * Cretes new leaves with given values.
+		 * 
+		 * @param color
+		 *            the color of the leaves
+		 * @param x
+		 *            x-coordinate
+		 * @param y
+		 *            y-coordinate
+		 * @param width
+		 *            the bottom width of the leaves
+		 * @param height
+		 *            the height of the leaves
+		 * @param topWidthPercent
+		 *            the top width in percent of {@code width} between 0 and 1
+		 * 
+		 * @since 1.0
+		 */
+		public Leaves(Color color, double x, double y, double width, double height, double topWidthPercent) {
+			p = new Polygon();
+
+			this.color = color;
+
+			size = new Point2D.Double(width, height);
+
+			pos = new Point2D.Double(x, y);
+
+			this.topWidthPercent = topWidthPercent;
+
+			updatePoly();
+		}
+
+		/**
+		 * Draws these leaves to {@code g2d}.
+		 * 
+		 * @param g2d
+		 *            the {@link Graphics2D} object to draw to
+		 * 
+		 * @since 1.0
+		 */
+		public void render(Graphics2D g2d) {
+			temp = g2d.getColor();
+			g2d.setColor(color);
+
+			g2d.fillPolygon(p);
+
+			g2d.setColor(temp);
+		}
+
+		/**
+		 * Updates the polygon.
+		 * 
+		 * @since 1.0
+		 */
+		public void updatePoly() {
+			p.reset();
+			p.addPoint((int) Math.round(pos.x), (int) Math.round(pos.y + size.y));
+			p.addPoint((int) Math.round(pos.x + size.x), (int) Math.round(pos.y + size.y));
+			p.addPoint((int) Math.round(pos.x + size.x * (1 - topWidthPercent)), (int) Math.round(pos.y));
+			p.addPoint((int) Math.round(pos.x + size.x * (0 + topWidthPercent)), (int) Math.round(pos.y));
+		}
+
+		/**
+		 * Sets the x and y coordinates of this leaves object.
+		 * 
+		 * @param x
+		 *            the x-coordinate
+		 * @param y
+		 *            the y-coordinate
+		 * 
+		 * @since 1.1
+		 */
+		public void setPos(double x, double y) {
+			pos.setLocation(x, y);
+			updatePoly();
+		}
 	}
 }
